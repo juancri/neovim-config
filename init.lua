49a60d0cf9d13a6d1049c9d887ec2f2389c851cb
JC Olivares <juancri@juancri.com>
2025-08-16 04:09:13 -0400

    Adding move up/down for insert mode

--- init.lua
+++ init.lua
@@ -7,3 +7,7 @@ vim.keymap.set('n', '<A-Down>', ':m .+1<CR>')
 vim.keymap.set('v', '<A-Up>', ":m '<-2<CR>gv")
 vim.keymap.set('v', '<A-Down>', ":m '>+1<CR>gv")
 
+-- Move up/down in Insert mode
+vim.keymap.set('i', '<A-Up>', '<Esc>:m .-2<CR>a')
+vim.keymap.set('i', '<A-Down>', '<Esc>:m .+1<CR>a')
+
